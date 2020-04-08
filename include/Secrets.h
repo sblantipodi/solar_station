d375bdb6866cf1f207271e220da13642bd8ff269
dperini <perini.davide@dpsoftware.org>
2020-04-08 20:59:00 +0200

    remove sec

--- include/Secrets.h
+++
@@ -1,7 +0,0 @@
-/****************** SECRETS ******************/
-const char* ssid = "XXX";
-const char* password = "XXX";
-const char* mqtt_server = "192.168.1.XX";
-const char* mqtt_username = "XXX";
-const char* mqtt_password = "XXX";
-const char* OTApassword =  "XXX"; //the password you will need to enter to upload remotely 
