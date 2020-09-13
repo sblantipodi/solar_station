517c4dec0545f6f896b22c040b18f7a27665945a
Davide Perini <perini.davide@dpsoftware.org>
2020-09-13 22:58:18 +0200

    second hardware iteration

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.5.13"
+        #define VERSION "1.5.11"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2020-09-13 21:54:38.771557"
+        #define BUILD_TIMESTAMP "2020-09-13 22:51:45.111242"
     #endif
     
