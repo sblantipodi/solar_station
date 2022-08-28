06e3a295e824358d1de6e6e6f78df1d06463c90b
Davide Perini <perini.davide@dpsoftware.org>
2022-08-28 14:12:56 +0200

    release v1.9.2

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.9.1"
+        #define VERSION "1.9.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-07-22 12:44:27.524739"
+        #define BUILD_TIMESTAMP "2022-08-28 14:12:21.408487"
     #endif
     
