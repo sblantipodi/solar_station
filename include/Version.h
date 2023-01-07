63e56c8c6f8e9e31ffdc2a26e8f15599c7d5ff3b
Davide Perini <perini.davide@dpsoftware.org>
2023-01-07 14:58:56 +0100

    esp8266/Arduino 3.1

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "1.9.3"
+        #define VERSION "1.9.4"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2022-12-29 20:41:56.957042"
+        #define BUILD_TIMESTAMP "2023-01-07 14:58:11.258354"
     #endif
     
