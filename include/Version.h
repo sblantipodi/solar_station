13d664ee0c3514146ba165f7fb88fb944ca9c71a
Davide Perini <perini.davide@dpsoftware.org>
2025-12-29 13:58:51 +0100

    Core 3

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "2.0.1"
+        #define VERSION "2.0.2"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2025-12-16 11:39:39.395800"
+        #define BUILD_TIMESTAMP "2025-12-16 15:59:42.662791"
     #endif
     
